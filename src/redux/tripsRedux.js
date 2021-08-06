76a771b19d8205353c920a0be3e5b139e5f475ce
Agnieszka Falandys <agnfal@gmail.com>
2021-08-06 08:01:35 +0200

    add views and filtering trips

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -12,9 +12,21 @@ export const getFilteredTrips = ({trips, filters}) => {
   }
 
   // TODO - filter by duration
-
+  if(filters.duration){
+	 console.log(filters.duration); console.log(output); 
+	 output = output.filter(trip => trip.days >= filters.duration.from && trip.days <= filters.duration.to);
+  }
+  
   // TODO - filter by tags
-
+  if(filters.tags.length>0){
+	let result = [];
+    
+	for(let tag of filters.tags){
+	  const trips = output.filter(trip => trip.tags.includes(tag));
+	  result = [...result, ...trips];
+	}
+	output = result;
+  }
   // TODO - sort by cost descending (most expensive goes first)
 
   return output;
@@ -24,18 +36,20 @@ export const getTripById = ({trips}, tripId) => {
   const filtered = trips;
 
   // TODO - filter trips by tripId
+  const result = filtered.filter(trip => trip.id === tripId);
 
   console.log('filtering trips by tripId:', tripId, filtered);
-  return filtered.length ? filtered[0] : {error: true};
+  return filtered.length ? result[0] : {error: true};
 };
 
 export const getTripsForCountry = ({trips}, countryCode) => {
   const filtered = trips;
 
   // TODO - filter trips by countryCode
-
+  const result = filtered.filter(trip => trip.country.code === countryCode);
+	
   console.log('filtering trips by countryCode:', countryCode, filtered);
-  return filtered.length ? filtered : [{error: true}];
+  return filtered.length ? result : [{error: true}];
 };
 
 /* ACTIONS */
